2212043e3bf13830d86451c4b5e6019f9dc23ffb
akik-hossain <akikhs00@gmail.com>
2023-08-15 15:53:12 +0600

    added navigation page

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ document.getElementById("btn-submit").addEventListener("click", function () {
 
   const passwordField = document.getElementById("user-password");
   const password = passwordField.value;
-  console.log(email, password);
+  if (email === "akikhs00@gmail.com" && password === "secret") {
+    window.location.href = "bank.html";
+  } else {
+    alert("Invalid User");
+  }
 });
